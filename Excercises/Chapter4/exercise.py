0af149506fa94999eed3bbffa1eba01acff2ca2d
NikolayZemelko <zemelko1989@gmail.com>
2023-01-02 15:39:58 +0300

    feat: func multy_map added
    The function is an array-based multiplication table. Each is multiplied with each. O(n*n) - Algorithm complexity

--- Excercises/Chapter4/exercise.py
+++ Excercises/Chapter4/exercise.py
@@ -1,2 +1,13 @@
-def multy_map(array: list) -> list: #list of lists
-    
+ARRAY = [2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def multy_map(array: list) -> list: # list of lists
+    return [[multiplier * num for multiplier in array] for num in array]
+
+# O(n*n) algorithm complexity
+
+def main():
+    print(multy_map(ARRAY))
+
+if __name__ == '__main__':
+    main()
